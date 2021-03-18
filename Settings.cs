575cd3561f36702bfc4c114aa7a169cd99b07a10
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2021-03-17 22:44:26 -0300

    [refactor] alterando secret para uma chave maior

--- Settings.cs
+++ Settings.cs
@@ -2,6 +2,6 @@ namespace Shop
 {
     public static class Settings
     {
-        public static string Secret = "secret-jwt";
+        public static string Secret = "2e0d4305-1b6e-4b30-a38d-775c7b999073";
     }
 }
