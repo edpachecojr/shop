0a22aab920e662d1347cdafe934fbc4dfb7cdc21
Edivaldo Pacheco Junior <edivaldopachecojunior@gmail.com>
2021-03-18 20:02:02 -0300

    [feature] CRUD de usuarios, autenticacao e autorizacao

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -13,6 +15,29 @@ namespace Shop.Controllers
     [Route("users")]
     public class UserController : ControllerBase
     {
+
+        [HttpGet]
+        [Route("")]
+        [Authorize(Roles = "manager")]
+        public async Task<ActionResult<List<User>>> Get([FromServices] DataContext context)
+        {
+            var users = await context
+                .Users
+                .AsNoTracking()
+                .ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        [Authorize(Roles = "manager")]
+        public async Task<ActionResult<User>> GetById(int id, [FromServices] DataContext context)
+        {
+            var user = await context.Users.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(user);
+        }
+
         [HttpPost]
         [AllowAnonymous]
         public async Task<ActionResult<User>> Post([FromBody] User model, [FromServices] DataContext context)
@@ -33,6 +58,29 @@ namespace Shop.Controllers
             }
         }
 
+        [HttpPut]
+        [Route("{id:int}")]
+        [Authorize(Roles = "manager")]
+        public async Task<ActionResult<User>> Put(int id, [FromServices] DataContext context, [FromBody] User model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (id != model.Id)
+                return NotFound(new { message = "Usuário não encontrado" });
+
+            try
+            {
+                context.Entry(model).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+                return model;
+            }
+            catch (System.Exception)
+            {
+                return BadRequest(new { message = "Não foi possível atualizar o usuário" });
+            }
+        }
+
         [HttpPost]
         [Route("login")]
         public async Task<ActionResult<dynamic>> Authenticate([FromServices] DataContext context, [FromBody] User model)
@@ -49,6 +97,26 @@ namespace Shop.Controllers
                 user = user,
                 token = token
             });
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        [Authorize(Roles = "manager")]
+        public async Task<ActionResult> Delete(int id, [FromServices] DataContext context)
+        {
+            var user = await context.Users.FirstOrDefaultAsync(x => x.Id == id);
+            if (user == null)
+                return NotFound(new { message = "Usuário não encontrado" });
+            try
+            {
+                context.Users.Remove(user);
+                await context.SaveChangesAsync();
+                return NoContent();
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível remover o usuário" });
+            }
         }
     }
 }
